3c2b9fe83a119c1f55e995b32b34007020433eee
rlcosta177 <rlcosta177@email.com>
2024-09-09 18:19:25 +0000

    add user interruption

--- fuzzer.py
+++ fuzzer.py
@@ -1,21 +1,26 @@
 import requests
 import sys
 
-for word in sys.stdin:
-    word = word.strip() # remove newlines and extra spaces from the wordlist.txt
+try:
+    for word in sys.stdin:
+        word = word.strip() # remove newlines and extra spaces from the wordlist.txt
 
-    try:
-        res = requests.get(url=f"https://example.com/{word}")
-        print(res) # print response status (e.g. <Response [200]> or <Response [404]>)
-        data = res.json() # parse response to json
-        print(data)
+        try:
+            res = requests.get(url=f"https://example.com/{word}")
+            print(res) # print response status (e.g. <Response [200]> or <Response [404]>)
+            data = res.json() # parse response to json
+            print(data)
 
-    # catch all request-related errors
-    except requests.exceptions.RequestException:
-        print(f"{{'{word}': Not Found'}}")
-        continue
+        # catch all request-related errors
+        except requests.exceptions.RequestException:
+            print(f"{{'{word}': Not Found'}}")
+            continue
 
-    # handle json decoding errors
-    except ValueError:
-        print(f"failed to decode json for {word}")
-        continue
+        # handle json decoding errors
+        except ValueError:
+            print(f"failed to decode json for {word}")
+            continue
+
+# handle user interruption (e.g. CTRL+C)
+except KeyboardInterrupt:
+    print("\nProcess interrupted by user.")
